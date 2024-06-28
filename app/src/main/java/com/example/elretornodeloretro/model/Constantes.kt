24f88b591cf2ba7eb238777d719b4a30d15f8dfb
JoseVicente <104432189+Unitato0315@users.noreply.github.com>
2024-06-28 11:43:45 +0200

    Update Constantes.kt

--- app/src/main/java/com/example/elretornodeloretro/model/Constantes.kt
+++ app/src/main/java/com/example/elretornodeloretro/model/Constantes.kt
@@ -4,5 +4,5 @@ object Constantes {
     const val SMTP_HOST = "smtp.gmail.com"
     const val SMTP_PORT = "587"
     const val EMAIL = "elretornodeloretro@gmail.com"
-    const val PASSWORD_APP = "awlk lnfu svbs tvrv"
+    const val PASSWORD_APP = "awlk lnfu svbs tvrv" // sustituir por la contraseña de aplicacion de tu correo
 }
